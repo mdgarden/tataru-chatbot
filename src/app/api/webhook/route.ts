fb607d6d4c5868e878428e6007148f2ba135c8bd
Madang <chooi9522@gmail.com>
2023-11-28 00:57:08 +0900

    메세지 고정 회신 함수 구현

---
+++ src/app/api/webhook/route.ts
@@ -0,0 +1,29 @@
+import * as line from "@line/bot-sdk";
+import crypto from "crypto";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const channelAccessToken = process.env.LINE_CHANNEL_ACCESS_TOKEN!;
+const channelSecret = process.env.LINE_CHANNEL_SECRET!;
+
+const client = new line.messagingApi.MessagingApiClient({
+  channelAccessToken,
+});
+
+const config = {
+  channelAccessToken,
+  channelSecret,
+};
+
+export async function GET() {
+  return Response.json({ id: 1 });
+}
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  const reply = client.replyMessage({
+    replyToken: body.events[0].replyToken,
+    messages: [{ type: "text", text: body.events[0].message.text }],
+  });
+
+  return Response.json(reply);
+}
