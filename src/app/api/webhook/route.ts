3d27e67d34346bb9da18e6d8c88772e56181b463
Madang <chooi9522@gmail.com>
2023-11-28 22:25:30 +0900

    [refactor] 타입 파일 생성, 각 파일 의존도 낮추기

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,21 +1,9 @@
 import * as line from "@line/bot-sdk";
 
-import {
-  COMMAND_LINKS,
-  COMMAND_NOTCE,
-  COMMAND_SEARCH,
-  COMMAND_QUICK_REPLY,
-} from "@/app/utils/consts";
-import { LINKS_TEMPLATE } from "@/app/utils/templates";
-import {
-  generateFlexMessage,
-  generateQuickReply,
-} from "@/app/utils/messageGenerators";
+import { generateMessage } from "@/app/utils/messageGenerators";
 
 const channelAccessToken = process.env.LINE_CHANNEL_ACCESS_TOKEN!;
 const channelSecret = process.env.LINE_CHANNEL_SECRET!;
-
-type ReplyMessage = line.messagingApi.Message;
 
 const client = new line.messagingApi.MessagingApiClient({
   channelAccessToken,
@@ -23,29 +11,15 @@ const client = new line.messagingApi.MessagingApiClient({
 
 export async function POST(req: Request) {
   const body = await req.json();
-  const command: string = body.events[0].message.text;
-  const generateMessage = (command: string): ReplyMessage => {
-    // TODO: 느낌표, 골뱅이 제외 특수문자 패스
+  const replyToken = body.events[0].replyToken;
+  const message = generateMessage(body.events[0].message.text);
 
-    const isQuickReply = !!COMMAND_QUICK_REPLY.find((word) =>
-      command.includes(word)
-    );
+  if (!message) return Response.json({});
 
-    if (command.startsWith(COMMAND_SEARCH))
-      return { type: "flex", text: "검색" }; // searchItem(command)
-    if (command === COMMAND_NOTCE)
-      return { type: "flex", text: generateFlexMessage(command) }; //getNewNotice()
-    if (command === COMMAND_LINKS)
-      return { type: "text", text: LINKS_TEMPLATE };
-    if (isQuickReply) return generateQuickReply(command);
-
-    return { type: "text", text: "" };
-  };
-
-  const reply = client.replyMessage({
-    replyToken: body.events[0].replyToken,
-    messages: [generateMessage(command)],
-  });
-
-  return Response.json(reply);
+  return Response.json(
+    client.replyMessage({
+      replyToken,
+      messages: [message],
+    })
+  );
 }
