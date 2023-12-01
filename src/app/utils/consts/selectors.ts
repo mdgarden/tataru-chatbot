59b9d0eb60a89a94386ad721f3fb63e1b7316247
Madang <chooi9522@gmail.com>
2023-12-02 00:57:01 +0900

    [refactor] consts 폴더 추가/유틸 정리

---
+++ src/app/utils/consts/selectors.ts
@@ -0,0 +1,3 @@
+export const NOTICELIST_SELECTOR =
+  ".toptabchanger_newsbox:first-child ul:nth-of-type(2) li";
+export const TOPICLIST_SELECTOR = ".news__list--topics_top li";
--- src/app/utils/helpers.ts
+++ src/app/utils/helpers.ts
@@ -1,3 +1,9 @@
+import { HELP_ME_YOSHIDA } from "./consts/commands";
+
 export const getRandomInt = (max: number) => {
   return Math.floor(Math.random() * max);
+};
+
+export const pickRandomYOSHIDA = () => {
+  return HELP_ME_YOSHIDA[getRandomInt(HELP_ME_YOSHIDA.length)];
 };
