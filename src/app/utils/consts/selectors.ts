4b9f2f749b27d787ef8179b23e7ecb14961100c2
Madang <chooi9522@gmail.com>
2023-12-03 01:47:28 +0900

    [feat] 멘테 본문스크랩 기능 추가, cheerio 함수 정리

--- src/app/utils/consts/selectors.ts
+++ src/app/utils/consts/selectors.ts
@@ -4,3 +4,5 @@ export const NOTICELIST_SELECTOR =
   ".toptabchanger_newsbox:first-child ul:nth-of-type(2) li";
 export const TOPICLIST_SELECTOR = ".news__list--topics_top li";
 export const EPOCHTIMER_SELECTOR = "ldst_strftime";
+export const NEWSDATE_SELECTOR = ".news__list--time";
+export const NOTICE_TEXT_SELECTOR = "div.news__detail__wrapper";
--- src/app/utils/scraper.ts
+++ src/app/utils/scraper.ts
@@ -3,65 +3,78 @@ import {
   EPOCHTIMER_SELECTOR,
   JP_LODESTONE_DOMAIN,
   LODESTONE,
+  NEWSDATE_SELECTOR,
   NOTICELIST_SELECTOR,
+  NOTICE_TEXT_SELECTOR,
   TOPICLIST_SELECTOR,
 } from "./consts/selectors";
 import { getLodestoneTime } from "./helpers";
+import { NoticeProps } from "./types";
 
-// news : 상위 카테고리
-// topic, notice : 하위 카테고리
-export const getGlobalNews = async () => {
-  // Fetch the HTML content of the web page to be scraped
-  const response = await fetch(JP_LODESTONE_DOMAIN + LODESTONE);
+type News = "topic" | "notice";
+
+async function loadHtml(targetUrl: string) {
+  const response = await fetch(targetUrl);
   const html = await response.text();
-
-  // Load the HTML content into Cheerio
   const $ = cheerio.load(html);
 
-  // Use Cheerio selectors to extract the desired data
-  const noticeList = $(NOTICELIST_SELECTOR)
-    .map((_, notice) => {
-      const date = $(notice)
-        .find(".news__list--time")
-        .text()
-        .split(EPOCHTIMER_SELECTOR)[1]
-        .match(/\d+/g);
+  return $;
+}
 
+function parseDate($: cheerio.CheerioAPI, elem: cheerio.Element) {
+  return $(elem)
+    .find(NEWSDATE_SELECTOR)
+    .text()
+    .split(EPOCHTIMER_SELECTOR)[1]
+    .match(/\d+/g);
+}
+
+async function getNoticeDetail(targetUrl: string) {
+  const $ = await loadHtml(targetUrl);
+
+  return $(NOTICE_TEXT_SELECTOR).text();
+}
+
+const getNews = async (newsType: News) => {
+  const isTopic = newsType === "topic";
+  const selector = isTopic ? TOPICLIST_SELECTOR : NOTICELIST_SELECTOR;
+  const $ = await loadHtml(JP_LODESTONE_DOMAIN + LODESTONE);
+
+  const parsedNews = $(selector)
+    .map(async (_, news) => {
+      const date = parseDate($, news as cheerio.Element);
       const formattedDate = date?.length && getLodestoneTime(date[0]);
+      const postUrl = JP_LODESTONE_DOMAIN + $(news).find("a").attr("href");
+      const text = isTopic
+        ? $(news).find("p.mdl-text__xs-m16").text()
+        : await getNoticeDetail(postUrl);
 
       return {
-        title: $(notice).find("p").text(),
+        title: $(news).find("p").text(),
         date: formattedDate,
-        postUrl: JP_LODESTONE_DOMAIN + $(notice).find("a").attr("href"),
-        text: "",
+        imgUrl: isTopic
+          ? $(news)
+              .find("img")
+              .attr("src")
+              ?.split(/.png|.jpg/)[0]
+          : "",
+        text,
+        postUrl,
       };
     })
     .toArray();
 
-  const topicList = $(TOPICLIST_SELECTOR)
-    .slice(0, 3)
-    .map((_, topic) => {
-      const date = $(topic)
-        .find(".news__list--time")
-        .text()
-        .split(EPOCHTIMER_SELECTOR)[1]
-        .match(/\d+/g);
+  return Promise.all(parsedNews);
+};
 
-      const formattedDate = date?.length && getLodestoneTime(date[0]);
+export const getGlobalTopics = async () => {
+  const topics = await getNews("topic");
 
-      return {
-        title: $(topic).find("a").first().text(),
-        date: formattedDate,
-        postUrl: JP_LODESTONE_DOMAIN + $(topic).find("a").first().attr("href"),
-        imgUrl: $(topic)
-          .find("img")
-          .attr("src")
-          ?.split(/.png|.jpg/)[0],
-        text: $(topic).find("p.mdl-text__xs-m16").text(),
-      };
-    })
-    .toArray();
+  return topics;
+};
 
-  // Return the scraped data as a JSON response
-  // res.status(200).json({ title, links });
+export const getGlobalNotices = async () => {
+  const notices = await getNews("notice");
+
+  return notices;
 };
